4cba27c816312648520b4c0de4c6bce54faeb2bf
bilalraiss <67203187+bilalraiss@users.noreply.github.com>
2021-09-13 08:41:11 -0700

    Update job_example.groovy

--- job_example.groovy
+++ job_example.groovy
@@ -1,4 +1,12 @@
 job('dsl-to-start') {
+  parameters {
+    stringParam {
+      name('PARAM_ONE')
+      defaultValue('value 1')
+      description('')
+      trim(false)
+    }
+  }
   steps {
     shell('echo "Hello"')
   }
